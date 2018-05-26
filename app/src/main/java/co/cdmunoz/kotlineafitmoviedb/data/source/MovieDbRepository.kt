d37354369cddea46c9c41d692718f92b7dfe0961
cdmunoz <_a_villa81_>
2018-05-25 21:25:50 -0500

    Making offline first - Creation of isConnected utilities

--- app/src/main/java/co/cdmunoz/kotlineafitmoviedb/data/source/MovieDbRepository.kt
+++ app/src/main/java/co/cdmunoz/kotlineafitmoviedb/data/source/MovieDbRepository.kt
@@ -1,18 +1,26 @@
 package co.cdmunoz.kotlineafitmoviedb.data.source
 
 import android.util.Log
+import co.cdmunoz.kotlineafitmoviedb.MoviesDbApplication
 import co.cdmunoz.kotlineafitmoviedb.data.MovieItem
 import co.cdmunoz.kotlineafitmoviedb.data.source.local.MoviesDbDao
 import co.cdmunoz.kotlineafitmoviedb.data.source.remote.MovieDbApiService
+import co.cdmunoz.kotlineafitmoviedb.utils.Utilities
 import io.reactivex.Observable
 
 
-class MovieDbRepository constructor(val apiService:MovieDbApiService, val moviesDbDao: MoviesDbDao) {
+class MovieDbRepository constructor(val apiService: MovieDbApiService,
+    val moviesDbDao: MoviesDbDao) {
 
-  fun getMovies(year: String, apiKey: String):Observable<List<MovieItem>>{
-    val observableFromApi = getMoviesFromApi(year, apiKey)
+  fun getMovies(year: String, apiKey: String): Observable<List<MovieItem>> {
+    val isConnected = Utilities.isConnectionAvailable(MoviesDbApplication.instance)
+    lateinit var observableFromApi: Observable<List<MovieItem>>
+    if (isConnected) {
+      observableFromApi = getMoviesFromApi(year, apiKey)
+    }
     val observableFromDb = getMoviesFromDb()
-    return Observable.concatArrayEager(observableFromApi, observableFromDb)
+    return if (isConnected) Observable.concatArrayEager(observableFromApi,
+        observableFromDb) else observableFromDb
   }
 
   private fun getMoviesFromApi(year: String, apiKey: String): Observable<List<MovieItem>> {
@@ -28,7 +36,7 @@ class MovieDbRepository constructor(val apiService:MovieDbApiService, val movies
         .doOnError { Log.e("API Insert", it.message) }
   }
 
-  fun getMoviesFromDb(): Observable<List<MovieItem>>{
+  fun getMoviesFromDb(): Observable<List<MovieItem>> {
     return moviesDbDao.queryMovies()
         .toObservable()
         .doOnNext {
