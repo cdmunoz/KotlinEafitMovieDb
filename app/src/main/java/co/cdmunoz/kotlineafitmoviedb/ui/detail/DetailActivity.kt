2c99295c2cfd1157b97fa983e254505b2021814d
cdmunoz <_a_villa81_>
2018-05-25 22:32:45 -0500

    Create new activity for details

---
+++ app/src/main/java/co/cdmunoz/kotlineafitmoviedb/ui/detail/DetailActivity.kt
@@ -0,0 +1,13 @@
+package co.cdmunoz.kotlineafitmoviedb.ui.detail
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import co.cdmunoz.kotlineafitmoviedb.R
+
+class DetailActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_detail)
+  }
+}
